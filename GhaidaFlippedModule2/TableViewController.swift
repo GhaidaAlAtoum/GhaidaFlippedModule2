f9661428b7ca7b75c692e92c4b037856fa486cd8
Ghaida Al-Atoum <ghaida.alatoum2@gmail.com>
2024-09-29 15:29:51 -0500

    [NT] - Init

---
+++ GhaidaFlippedModule2/TableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TableViewController.swift
+//  GhaidaFlippedModule2
+//
+//  Created by Ghaida Atoum on 9/29/24.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+}
--- GhaidaFlippedModule2/ViewController.swift
+++ GhaidaFlippedModule2/ViewController.swift
@@ -9,11 +9,109 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userView: UIView!
+    struct AudioConstants{
+        static let AUDIO_BUFFER_SIZE = 1024*4
+    }
+    
+    // setup audio model
+    let audio = AudioModel(buffer_size: AudioConstants.AUDIO_BUFFER_SIZE)
+    lazy var graph:MetalGraph? = {
+        return MetalGraph(userView: self.userView)
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        print("View loaded")
+        if let graph = self.graph{
+            graph.setBackgroundColor(r: 0, g: 0, b: 0, a: 1)
+            
+            // add in graphs for display
+            // note that we need to normalize the scale of this graph
+            // because the fft is returned in dB which has very large negative values and some large positive values
+            
+            // BONUS: lets also display a version of the FFT that is zoomed in
+//            graph.addGraph(withName: "fftZoomed",
+//                            shouldNormalizeForFFT: true,
+//                            numPointsInGraph: 300) // 300 points to display
+            
+            
+            graph.addGraph(withName: "fft",
+                            shouldNormalizeForFFT: true,
+                            numPointsInGraph: AudioConstants.AUDIO_BUFFER_SIZE/2)
+            
+            graph.addGraph(withName: "time",
+                numPointsInGraph: AudioConstants.AUDIO_BUFFER_SIZE)
+            
+            
+            
+            graph.makeGrids() // add grids to graph
+        }
+        
+        // start up the audio model here, querying microphone
+        audio.startMicrophoneProcessing(withFps: 20) // preferred number of FFT calculations per second
 
+        audio.play()
+        
+        // run the loop for updating the graph peridocially
+        Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { _ in
+            self.updateGraph()
+        }
+       
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("View Will Disapper")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("View Will Appear")
+    }
+    
+    override func viewIsAppearing(_ animated: Bool) {
+        print("View is appearing")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("view did appear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("View Did Disapper")
+    }
+    
+    // periodically, update the graph with refreshed FFT Data
+    func updateGraph(){
+        
+        if let graph = self.graph{
+            graph.updateGraph(
+                data: self.audio.fftData,
+                forKey: "fft"
+            )
+            
+            graph.updateGraph(
+                data: self.audio.timeData,
+                forKey: "time"
+            )
+            
+            // BONUS: show the zoomed FFT
+            // we can start at about 150Hz and show the next 300 points
+            // actual Hz = f_0 * N/F_s
+//            let startIdx:Int = 150 * AudioConstants.AUDIO_BUFFER_SIZE/audio.samplingRate
+//            let subArray:[Float] = Array(self.audio.fftData[startIdx...startIdx+300])
+//            graph.updateGraph(
+//                data: subArray,
+//                forKey: "fftZoomed"
+//            )
+            //TODO: should this be here in the View controller? Or in Model?
+            
+        }
+        
+    }
 
 }
 
